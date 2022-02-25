ff7468adc659f9a7cf3cc4360e622c58281e283b
Kirby Hines <kirbyhines@Kirbys-MBP.local>
2022-02-25 12:49:15 -0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['links.papareact.com', 'image.tmdb.org'],
+  },
+};
